246bd8c4aa0be8849be607c56190536ac5fbabfa
Holger Leerhoff <59140808+TrismegistusS@users.noreply.github.com>
2021-12-21 13:48:14 +0100

    Messwerte hinzugefügt, Typos, Kompatibilität

--- DWD_hist_weather.py
+++ DWD_hist_weather.py
@@ -5,7 +5,7 @@
 #
 # (c) 2021 Holger Leerhoff
 #
-# Dieses Modulk importiert Daten aus dem umfangreichen OpenData-Angebot
+# Dieses Modul importiert Daten aus dem umfangreichen OpenData-Angebot
 # des Deutschen Wetterdienstes (DWD) in ein Pandas-Dataframe.
 #
 # Regionalität
@@ -34,7 +34,8 @@ def tageswerte_land(auswertungsland, still=False):
 
     Returns
     -------
-    Pandas DataFrame with the state's daily average measured values
+    Pandas DataFrame with the state's daily average measured values:
+    - TempMean, HumidityMean, TempMax, TempMin, SunshineDuration
     """
     DWD_PFAD = 'https://opendata.dwd.de/climate_environment/CDC/' \
                'observations_germany/climate/daily/kl/'
@@ -42,9 +43,9 @@ def tageswerte_land(auswertungsland, still=False):
                     'Bremen', 'Hamburg', 'Hessen', 'Mecklenburg-Vorpommern',
                     'Niedersachsen', 'Nordrhein-Westfalen', 'Rheinland-Pfalz',
                     'Saarland', 'Sachsen', 'Sachsen-Anhalt',
-                    'Schleswig-Holsetin', 'Thüringen'], 'Bitte Namem eines Bundeslands' \
+                    'Schleswig-Holstein', 'Thüringen'], 'Bitte Namem eines Bundeslands' \
                     ' als Parameter übergeben.'
-    # %% Laden der Wetterstationen vom DWD-OpenData-Server. Infos, Datensatz-
+    # Laden der Wetterstationen vom DWD-OpenData-Server. Infos, Datensatz-
     # beschreibung etc. hier: https://opendata.dwd.de/README.txt
     url = DWD_PFAD + 'historical/KL_Tageswerte_Beschreibung_Stationen.txt'
     stationen = pd.DataFrame()
@@ -61,7 +62,7 @@ def tageswerte_land(auswertungsland, still=False):
     for land in stationen['Bundesland'].unique():
         stationen_ids[land] = stationen[stationen['Bundesland']
                                         == land]['Stations_id'].tolist()
-    # %% Zusammenstellen der URLs der ZIP-Archive der Wetterstationen vom
+    # Zusammenstellen der URLs der ZIP-Archive der Wetterstationen vom
     # DWD-OpenData-Server. Aufrufen der html-Seite, parsen mit BeautifulSoup,
     # die entsprechenden URLs in einer Liste speichern.
     stationen_dateinamen = {}
@@ -70,14 +71,19 @@ def tageswerte_land(auswertungsland, still=False):
     for node in soup.find_all('a'):
         if node.get('href').startswith('tageswerte_KL_'):
             stationen_dateinamen[int(node.text[14:19])] = node.text
-    # %% Die Wetterdaten ausgewählter Wetterstationen (als ZIP-Archiv) vom
+    # Die Wetterdaten ausgewählter Wetterstationen (als ZIP-Archiv) vom
     # DWD-OpenData-Server ziehen, darin die eigentliche Datendatei finden und
     # deren Inhalte einlesen.
     # Zu allen Wetterstationen eine Datei mit aktuellen Wetterdaten suchen und
     # deren Inhalte einlesen.
     # Error-Handling für Stationen ohne freie / akt. Daten.
-    # NB: Hier wird die Temperatur (TMK) ausgelesen, Modifikaton für andere
-    # Messwerte sind leicht möglich.
+    # Hier werden ausgelesen:
+    #  - das Tagesmittel der Temperatur in °C (TMK)
+    #  - das Tagesmittel der relativen Feuchte in % (UPM)
+    #  - das Tagesmaximum der Temeratur in 2m Höhe in °C (TXK)
+    #  - das Tagesminimum der Temeratur in 2m Höhe in °C (TNK)
+    #  - die tägliche Sonnenscheindauer in h (SDK)
+    # Im Datensatz sind noch weitere Messwerte vorhanden.
     wetter = pd.DataFrame()
     for station in stationen_ids[auswertungsland]:
         for typ in ['historical', 'recent']:
@@ -90,12 +96,17 @@ def tageswerte_land(auswertungsland, still=False):
                 gezippte_dateien = ZipFile(BytesIO(urlopen(url).read()))
                 csv_dateiname = fnmatch.filter(gezippte_dateien.namelist(),
                                                'produkt*.txt')
+                # TODO: bei Protokollwunsch hier speichern
                 csv_daten = gezippte_dateien.open(*csv_dateiname)
                 wetter = wetter.append(pd.read_csv(csv_daten,
                                                    sep=';',
                                                    usecols=['STATIONS_ID',
                                                             'MESS_DATUM',
-                                                            ' TMK'],
+                                                            ' TMK',
+                                                            ' UPM',
+                                                            ' TXK',
+                                                            ' TNK',
+                                                            ' SDK'],
                                                    parse_dates=['MESS_DATUM']))
                 if not still:
                     print('.', end='')
@@ -108,16 +119,21 @@ def tageswerte_land(auswertungsland, still=False):
     # Missings (-999.0) durch System-Missings ersetzen.
     wetter = (wetter.rename(columns={'STATIONS_ID': 'Station',
                                      'MESS_DATUM': 'Datum',
-                                     ' TMK': 'Temp'})
+                                     ' TMK': 'TempMean',
+                                     ' UPM': 'HumidityMean',
+                                     ' TXK': 'TempMax',
+                                     ' TNK': 'TempMin',
+                                     ' SDK': 'SunshineDuration'})
                     .replace(-999.0, np.nan))
-    # %% Aus Stationsdaten regionale Tagesmittelwerte bilden
-    tageswerte = wetter[['Datum', 'Temp']].groupby('Datum').mean()
+    # Aus Stationsdaten regionale Tagesmittelwerte bilden
+    tageswerte = wetter[['Datum', 'TempMean', 'HumidityMean', 'TempMax', 'TempMin',
+                         'SunshineDuration']].groupby('Datum').mean()
     tageswerte['Jahr'] = tageswerte.index.year
     tageswerte['Monat'] = tageswerte.index.month
     tageswerte['Tag_des_Jahres'] = tageswerte.index.dayofyear
     return tageswerte
 
-def tagestemp_land(auswertungsland, still=False):  # aus Kompatibilitätsgründen noch drin
+def tagestemp_land(auswertungsland, still=False):  # for backwards compatibility
     """
     Parameters
     ----------
@@ -128,91 +144,15 @@ def tagestemp_land(auswertungsland, still=False):  # aus Kompatibilitätsgründe
     -------
     Pandas DataFrame with the state's daily average temeratures
     """
-    DWD_PFAD = 'https://opendata.dwd.de/climate_environment/CDC/' \
-               'observations_germany/climate/daily/kl/'
-    if auswertungsland not in ['Baden-Württemberg', 'Bayern', 'Berlin', 'Brandenburg',
-                    'Bremen', 'Hamburg', 'Hessen', 'Mecklenburg-Vorpommern',
-                    'Niedersachsen', 'Nordrhein-Westfalen', 'Rheinland-Pfalz',
-                    'Saarland', 'Sachsen', 'Sachsen-Anhalt',
-                    'Schleswig-Holsetin', 'Thüringen']:
-        raise ValueError
-    # %% Laden der Wetterstationen vom DWD-OpenData-Server. Infos, Datensatz-
-    # beschreibung etc. hier: https://opendata.dwd.de/README.txt
-    url = DWD_PFAD + 'historical/KL_Tageswerte_Beschreibung_Stationen.txt'
-    stationen = pd.DataFrame()
-    stationen = pd.read_fwf(url,
-                            encoding='ISO-8859-1',
-                            header=None,
-                            skiprows=[0, 1],
-                            names=['Stations_id', 'von_datum', 'bis_datum',
-                                   'Stationshoehe', 'geoBreite', 'geoLaenge',
-                                   'Stationsname', 'Bundesland'])
-    # Aus dem Datensatz alle Stations-IDs nach Ländern extrahieren und als
-    # separate Listen mit den Ländernamen als Schlüssel in dictionary packen.
-    stationen_ids = {}
-    for land in stationen['Bundesland'].unique():
-        stationen_ids[land] = stationen[stationen['Bundesland']
-                                        == land]['Stations_id'].tolist()
-    # %% Zusammenstellen der URLs der ZIP-Archive der Wetterstationen vom
-    # DWD-OpenData-Server. Aufrufen der html-Seite, parsen mit BeautifulSoup,
-    # die entsprechenden URLs in einer Liste speichern.
-    stationen_dateinamen = {}
-    page = requests.get(DWD_PFAD+'historical').text
-    soup = BeautifulSoup(page, 'html.parser')
-    for node in soup.find_all('a'):
-        if node.get('href').startswith('tageswerte_KL_'):
-            stationen_dateinamen[int(node.text[14:19])] = node.text
-    # %% Die Wetterdaten ausgewählter Wetterstationen (als ZIP-Archiv) vom
-    # DWD-OpenData-Server ziehen, darin die eigentliche Datendatei finden und
-    # deren Inhalte einlesen.
-    # Zu allen Wetterstationen eine Datei mit aktuellen Wetterdaten suchen und
-    # deren Inhalte einlesen.
-    # Error-Handling für Stationen ohne freie / akt. Daten.
-    # NB: Hier wird die Temperatur (TMK) ausgelesen, Modifikaton für andere
-    # Messwerte sind leicht möglich.
-    wetter = pd.DataFrame()
-    for station in stationen_ids[auswertungsland]:
-        for typ in ['historical', 'recent']:
-            try:
-                if typ == 'historical':
-                    url = DWD_PFAD+'historical/'+stationen_dateinamen[station]
-                else:
-                    url = DWD_PFAD+'recent/tageswerte_KL_' + \
-                                   str(station).zfill(5)+'_akt.zip'
-                gezippte_dateien = ZipFile(BytesIO(urlopen(url).read()))
-                csv_dateiname = fnmatch.filter(gezippte_dateien.namelist(),
-                                               'produkt*.txt')
-                csv_daten = gezippte_dateien.open(*csv_dateiname)
-                wetter = wetter.append(pd.read_csv(csv_daten,
-                                                   sep=';',
-                                                   usecols=['STATIONS_ID',
-                                                            'MESS_DATUM',
-                                                            ' TMK'],
-                                                   parse_dates=['MESS_DATUM']))
-                if not still:
-                    print('.', end='')
-            except KeyError:  # für die Wetterstation liegen keine Daten vor
-                if not still:
-                    print('-', end='')
-            except IOError:  # für die Wetterstation liegen keine akt. Daten vor
-                if not still:
-                    print('-', end='')
-    # Missings (-999.0) durch System-Missings ersetzen.
-    wetter = (wetter.rename(columns={'STATIONS_ID': 'Station',
-                                     'MESS_DATUM': 'Datum',
-                                     ' TMK': 'Temp'})
-                    .replace(-999.0, np.nan))
-    # %% Aus Stationsdaten regionale Tagesmittelwerte bilden
-    tageswerte = wetter[['Datum', 'Temp']].groupby('Datum').mean()
-    tageswerte['Jahr'] = tageswerte.index.year
-    tageswerte['Monat'] = tageswerte.index.month
-    tageswerte['Tag_des_Jahres'] = tageswerte.index.dayofyear
+    tageswerte = tageswerte_land(auswertungsland)
+    tageswerte = tageswerte[['TempMean', 'Jahr', 'Monat', 'Tag_des_Jahres']]
+    tageswerte = tageswerte.rename(columns={'TempMean': 'Temp'})
     return tageswerte
 
 # beim direkten Aufruf mit Land als Parameter jährlichen Temperaturdurchschnitt ausgeben
 if __name__ == "__main__":
     import sys
     tageswerte = tageswerte_land(sys.argv[1])
-    print(f'\nJähliche Durchschnittstempertauren für {sys.argv[1]}.')
-    print(tageswerte.groupby('Jahr')['Temp'].mean())
+    print(f'\nJähliche Durchschnittstemperturen für {sys.argv[1]}.')
+    print(tageswerte.groupby('Jahr')['TempMean'].mean())
         
